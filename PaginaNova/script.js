b76c515e174da8eb13f2ea2ac73d4f2106ab082d
Erik Domnick <erik.maciel.santos@escola.pr.gov.br>
2026-03-30 10:20:07 -0300

    sei lá

---
+++ PaginaNova/script.js
@@ -0,0 +1,21 @@
+// tipo number
+//numero flutuante 
+const meuNumero = 3;
+console.log(meuNumero);
+
+const primeironumero = 1;
+const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log('resultado'operacaoMatematica);
+ 
+//ponto flutuante
+const numeroFluente = 3.3;
+const pontoFlutuantesemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroFluente;
+console.log(novaOperacao.toFixed(2));
+//toFixed() permite formatar o 
+
+// NaN = Not a Number
+const nome ='celv'
+console.log(nome * primeironumero)
