cc1562ce9c6e28427fbd9d17631d2c7d87ced191
Erik Domnick <erik.maciel.santos@escola.pr.gov.br>
2026-03-30 09:33:59 -0300

    testando

---
+++ PaginaNova-main/script.js
@@ -0,0 +1,9 @@
+//tipo numero
+//inteiro ou com pontos flutuantes (float ou real)
+const meuNumero = 3;
+console.log(meuNumero);
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log('resultado = '+operacaoMatematica) 
