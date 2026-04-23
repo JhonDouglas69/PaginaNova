8d93f429073c2f00d2c4c38ac979558de84e10c2
JhonDouglas69 <jhon.colque@escola.pr.gov.br>
2026-04-23 08:48:17 -0300

    popo

---
+++ aula7.js
@@ -0,0 +1,23 @@
+//tipagem dinamica
+//nao precisa definir o tipo de dado de cada
+//variavel ou constante tera.
+
+var num = 777;
+let ligado = 666;
+ligado = 6767;
+
+//Java Script
+//Linguagem MuliparaDigma - resolver o mesmo problema utilizando diferentes abordagens
+//ES6 /ES2015 - Versão do Js lançada em 2015 com atualização constante
+
+//Linguagem interpretada - vai analisando linha a linha diferente da linguagem compilada que gera um arquivo de maquina para executar. É mais rapido
+
+//NodeJS - nao e uma linguagem (é o JS) 
+//interpretador de JS 
+//O JS,por ser uma linguagem
+//interpretada , precisa de um interpretador!
+
+//Podemos utilizar o navegador como interpretador
+
+//NodeJS norrmalmente é utilizado no Back-and
+//Deno é alternativa ao NodeJS
--- conversoes.js
+++ conversoes.js
@@ -4,8 +4,18 @@
 //Boolean = lógico
 
 //Conversões implicitas
-const numero = 456
-const numString =
+const numero = 456;
+const numString = '456';
+console.log(numero == numString);//converter um numero String e compara
+// == conversoes implicitas
 
 //Conversões esplícitas
+//deve-se utilizar funçoes de conversoes para cada tipo
+console.log(numero + numString); //concatenação
+//EX:
+//'J'+'h'+'o'+'n' = jhon
+console.log(numero + Number(numString));//converçao esplicita
+console.log(String(numero) + numString);//converçao esplicita
 
+ const numString2 = '456a'; //erro de converçao 456a nao pode ser converdido para numero
+ console.log(numero + Number(numString2));
