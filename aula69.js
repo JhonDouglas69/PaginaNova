e595ee897a59e8479f0c3bb25a6e828326d9fb02
JhonDouglas69 <jhon.colque@escola.pr.gov.br>
2026-05-04 08:52:28 -0300

    lula10

--- aula69.js
+++ aula69.js
@@ -1,1 +1,18 @@
-//console é uma ferramenta que pode ser encontrada nos navegadores e no NodeJS log éum registro
+//console é uma ferramenta que pode ser 
+//encontrada nos navegadores e no NodeJS 
+//log é um registro
+
+const minhaVAR = true;
+console.log(245);
+console.log(minhaVAR);
+console.log("Eu sou um pedido");
+
+//Tratamento de e co
+console.log('erro')
+/*Existem outros metodos de utilização do console
+console.erro();para exibir mensagem de erro
+console.table();para visualizar de forma mais 
+organizada e informações tabulares.
+console.time();e console.timeEnd(); //para temporarizar uma operação do codigo leva para se iniciada e concluida
+console.trace();//para exibir a stacktrack de todos os pontos 
+(ou seja arquivos chamados) por onde o codigo executado passou durante a execução
