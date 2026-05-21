a80abdc9faaf112a4fee1d89a1129e5c3cd80634
JhonDouglas69 <jhon.colque@escola.pr.gov.br>
2026-05-21 12:16:30 -0300

    Ctrl S

---
+++ controlS.js
@@ -0,0 +1,23 @@
+/*let idadeMin = 18;
+let idadeMax = 100;
+
+if (idadeMax >= idadeMin )
+console.log("vc pode fazer a carteira de abilitaçao")
+
+//Operador Ternario                    
+//Condição ? resposta V : respostaF
+let permissao = idadeMin >= idadeMax ? sim : não;
+console.log(selecao == 'brasil'?'O hexa é nosso?':'Ficou pra proxima!');
+
+
+//Outro Exemplo
+let selecao = 'brasil';
+console.log(selecao == 'brasil'?'O hexa é nosso?':'Ficou pra proxima!');
+
+/*Calcule o valor da compra de varios itens de acordo com 
+    1 item = 5,00 cada
+    +3 itens = 4,00 cada
+*/
+let itens = 3;
+let preco = itens > 3 ? 4.00 : 5.00;
+console.log('valor da compra é de :'+ itens * preco);
